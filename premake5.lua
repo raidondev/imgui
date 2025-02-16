95d4fb81fe6a83da3c6b15707475650e55c4a864
Don Landfair <donlandfair@gmail.com>
2025-02-15 18:17:12 -0600

    Added premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,29 @@
+project "ImGui"
+    kind "StaticLib"
+    language "C++"
+
+    targetdir ("Binaries/" .. outputdir .. "/%{prj.name}")
+    objdir ("Binaries/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "imconfig.h",
+        "imgui.h",
+        "imgui.cpp",
+        "imgui_draw.cpp",
+        "imgui_internal.h",
+        "imgui_tables.cpp",
+        "imgui_widgets.cpp",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h",
+        "imgui_demo.cpp",
+    }
+
+    filter "system:windows"
+    systemversion "latest"
+    cppdialect "C++20"
+    staticruntime "On"
+
+    filter { "system:windows", "configurations:Release" }
+        buildoptions { "/MT", "/utf-8" }
