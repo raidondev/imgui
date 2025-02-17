54a65afd38cc54bd58a289f3fce41dc69c6e4e5e
Vadonis Landfair <vadonis@digitalphxgames.com>
2025-02-17 12:38:40 -0600

    Switched ImGui to docking branch; Nyx Engine build as static lib & fixed warnings.

--- premake5.lua
+++ premake5.lua
@@ -23,7 +23,7 @@ project "ImGui"
     filter "system:windows"
     systemversion "latest"
     cppdialect "C++20"
-    staticruntime "On"
+    staticruntime "on"
 
     filter { "system:windows", "configurations:Release" }
         buildoptions { "/MT", "/utf-8" }
